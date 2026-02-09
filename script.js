0fd32d31d51fc45b2930a011e336cf9221632b22
Mafikul Islam <mafikul.islam.raj.bd@gmail.com>
2026-02-09 14:58:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,30 +7,14 @@ function updatePreview() {
     };
 
     for (let key in fields) {
-        const val = document.getElementById(key).value;
-        document.getElementById(fields[key]).innerText = val;
+        const input = document.getElementById(key);
+        const output = document.getElementById(fields[key]);
+        if (input && output) output.innerText = input.value;
     }
-    // Update the second exp no inside table
     document.getElementById('expNoOutTable').innerText = document.getElementById('expNoIn').value;
 }
 
-function saveLogo(input, imgId) {
-    const file = input.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            document.getElementById(imgId).src = e.target.result;
-            localStorage.setItem(imgId, e.target.result);
-        };
-        reader.readAsDataURL(file);
-    }
-}
-
 window.onload = () => {
-    ['leftLogoImg', 'rightLogoImg'].forEach(id => {
-        const data = localStorage.getItem(id);
-        if (data) document.getElementById(id).src = data;
-    });
     updatePreview();
 };
 
@@ -38,7 +22,7 @@ function downloadPDF() {
     const element = document.getElementById('cover-page');
     const opt = {
         margin: 0,
-        filename: 'Lab_Report_Cover.pdf',
+        filename: 'Varendra_University_Report.pdf',
         image: { type: 'jpeg', quality: 1 },
         html2canvas: { scale: 3, useCORS: true },
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
@@ -46,7 +30,6 @@ function downloadPDF() {
     };
 
     html2pdf().set(opt).from(element).toPdf().get('pdf').then(function (pdf) {
-        // Double check to ensure only 1 page remains
         const pageCount = pdf.internal.getNumberOfPages();
         if (pageCount > 1) {
             for (let i = pageCount; i > 1; i--) {
